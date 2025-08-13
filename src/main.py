f26b659cdcfc28767dc1579d36920850bf1ebfb9
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-13 16:33:41 +0700

    refactor: remove unused logging initialization and handlers
    eliminated the init_logging call and commented out exception handler registration to streamline the main application setup

--- src/main.py
+++ src/main.py
@@ -3,34 +3,14 @@ from fastapi import FastAPI
 
 from src.custom import LoggingMiddleware
 from src.custom.cst_lifespan import app_lifespan  # lifespan pakai DataService
-from src.mlogg import init_logging
 from src.router import register_routes
 
-init_logging()
 app = FastAPI(lifespan=app_lifespan)
 
 # Mask sensitive fields di logs
 app.add_middleware(LoggingMiddleware, mask_fields=["password", "token", "secret"])
 
 register_routes(app)
-
-
-# # Register exception handlers
-# def register_exception_handlers(app):
-#     @app.exception_handler(AppExceptionError)
-#     async def app_exception_handler(request: Request, exc: AppExceptionError):
-#         logger.exception(f"Exception occurred: {exc}")
-#         return JSONResponse(
-#             status_code=exc.status_code,
-#             content={
-#                 "error": exc.name,
-#                 "message": exc.message,
-#                 "context": exc.context,
-#             },
-#         )
-
-
-# register_exception_handlers(app)
 
 
 # Root endpoint
