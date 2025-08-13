cac4d667871a25433e4866f19fd3ef385ae8724b
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-13 19:25:41 +0700

    feat: add member and module authentication services

---
+++ src/service/auth/__init__.py
@@ -0,0 +1,7 @@
+from src.service.auth.srv_memberauth import MemberAuthService
+from src.service.auth.srv_moduleauth import ModuleAuthService
+
+__all__ = [
+    "MemberAuthService",
+    "ModuleAuthService",
+]
