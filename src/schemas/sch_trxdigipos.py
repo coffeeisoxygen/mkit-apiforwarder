e7b42d81a7d581ecd818fa4f71df14e76fe38393
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-14 14:23:09 +0700

    feat: enhance Digipos transaction models with validation
    added common issues section to documentation, improved moduleid description, and introduced col field for column specification in DigiposTrxModelList

--- src/schemas/sch_trxdigipos.py
+++ src/schemas/sch_trxdigipos.py
@@ -1,9 +1,19 @@
 """class untuk bertransaksi digipos.
 
 inherit dari base model, TrxRequestBase, dan untuk response transaksi juga menggunakan TrxResponseBase.
+
+common issues:
+- jika tanpa api ini adalah :
+    - exposing credentials : username, pin , password dari akun API
+- dengan api ini:
+    - trx otomax ke api akan di validasi dengan pin passwod / sign
+    - moduleid : credentials akun API yg di simpan di yaml | switch
+    - product : equivalen dengan category yg ada di API Digipos
+    - subproduct: equivalen dengan productSubCategory
+    - support all params actual dari API Digipos ASL.
 """
 
-from pydantic import Field, field_validator
+from pydantic import BaseModel, Field, field_validator
 
 from src.schemas.sch_trxbase import TrxAuthFields, TrxBaseModel
 from src.utils.enumloader import get_enum, load_enums_from_yaml
@@ -14,11 +24,15 @@ PaymentEnum = get_enum(enums, "PaymentEnums")
 
 
 class DigiposTrxModel(TrxBaseModel, TrxAuthFields):
-    """Model untuk transaksi Digipos."""
+    """Model untuk transaksi Digipos.
 
-    moduleid: str = Field(..., description="ID modul Account transaksi Digipos")
-    markup: str | int | None = Field(
-        0, description="Markup harga untuk transaksi Digipos, default 0"
+    moduleid adalah data module digios yg ada di yaml
+    ini wajib udah ada.
+    """
+
+    moduleid: str = Field(
+        ...,
+        description="ID modul Account API Digipos, ini equvalen dengan username=<username>",
     )
 
     @field_validator("product", mode="before")
@@ -28,6 +42,15 @@ class DigiposTrxModel(TrxBaseModel, TrxAuthFields):
         if value_upper not in ProductEnum._value2member_map_:
             raise ValueError(f"Invalid product: {value}")
         return value_upper
+
+
+class DigiposMarkup(BaseModel):
+    """Model untuk markup transaksi Digipos."""
+
+    markup: str | int | None = Field(
+        default=0,
+        description="Markup untuk transaksi,ini equivalen dengan up_harga=<markup>",
+    )
 
     @field_validator("markup", mode="before")
     @classmethod
@@ -40,3 +63,23 @@ class DigiposTrxModel(TrxBaseModel, TrxAuthFields):
             except ValueError:
                 return 0
         return value
+
+
+class DigiposTrxModelList(DigiposTrxModel, DigiposMarkup):
+    """Model untuk transaksi check List Paket Eligible.
+
+    endpoint:
+        yang akan menggunakan ini adalah /digipos/list?
+    """
+
+    subproduct: str | None = Field(
+        description="ini equivalen dengan productSubCategory=<subproduct>"
+    )
+    duration: str | None = Field(description="ini equivalen dengan duration=<duration>")
+    pm: str = Field(
+        description="Payment method yang di gunakan,equivalen dengan payment_method=<payment_method>"
+    )
+    col: list[str] | None = Field(
+        default=["productId,productSubCategory,productName,quota,duration,total_"],
+        description="List kolom yang di gunakan, ini equivalen dengan kolom=productId,productSubCategory,duration, dan lain lain.",
+    )
